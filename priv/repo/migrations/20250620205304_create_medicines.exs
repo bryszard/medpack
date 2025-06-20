f1a592549ea033f94389e44faab474e979f9e99d
Fly Dev <dev@fly.io>
2025-06-20 20:53:10 +0000

    create medicines table migration

---
+++ priv/repo/migrations/20250620205304_create_medicines.exs
@@ -0,0 +1,19 @@
+defmodule MedicineInventory.Repo.Migrations.CreateMedicines do
+  use Ecto.Migration
+
+  def change do
+    create table(:medicines) do
+      add :name, :string, null: false
+      add :type, :string
+      add :quantity, :integer
+      add :expiration_date, :date
+      add :photo_path, :string
+      add :notes, :text
+
+      timestamps()
+    end
+
+    create index(:medicines, [:name])
+    create index(:medicines, [:expiration_date])
+  end
+end
